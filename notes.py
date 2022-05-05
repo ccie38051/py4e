8c6862dd939c683a2a83ddbad4b38d16c65eabeb
jerm <ccie323@gmail.com>
2022-05-05 07:11:07 -0700

    commit on 'master'

---
+++ notes.py
@@ -0,0 +1,52 @@
+#####PYTHON TRAINING CHUCK
+
+# INSTALL
+ - Windows, Mac or Linux
+ - Install python3
+ - create folder
+ - Install IDE - ATOM works for all OS, same VSC
+ - Create file first.py in directory
+ - Run file from directory or CLI
+ - Create note files in python
+
+ Python is basically words, sentences & paragraphs of code
+ reserved words for python:
+ False
+ None
+ True
+ and
+ as
+ assert
+ break
+ class
+ continue
+ def
+ del
+ elif
+ else
+ except
+ finally
+ for
+ from
+ global
+ if
+ import
+ in
+ is
+ lambda
+ nonlocal
+ not
+ or
+ pass
+ raise
+ return
+ try
+ while
+ with
+ yield 
+
+
+# VARIABLES, EXPRESSION & STATEMENTS
+
+
+test
